20c4c87ec0ca5f6885de6d049052056fc2ccdf50
yyfyyfstudy1 <yifan.yu@uqconnect.edu.au>
2024-05-11 23:37:24 +0800

    test file

--- src/test/java/com/yupi/sqlfather/DruidParserTest.java
+++ src/test/java/com/yupi/sqlfather/DruidParserTest.java
@@ -18,9 +18,9 @@ public class DruidParserTest {
             + "(\n"
             + "username varchar(256) not null comment '用户名',\n"
             + "id bigint not null auto_increment comment '主键' primary key,\n"
-            + "create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',\n"
             + "update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',\n"
             + "is_deleted tinyint default 0 not null comment '是否删除（0-未删，1-已删）'\n"
+            + "create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',\n"
             + ") comment '用户表';";
 
     @Test
